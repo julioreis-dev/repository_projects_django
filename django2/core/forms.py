ebe99c3587229b167c66f6100399ba6d52b0e451
Julio dos Reis Firmino <67196397+julioreis-dev@users.noreply.github.com>
2021-03-10 22:59:21 -0300

    Add files via upload

---
+++ django2/core/forms.py
@@ -0,0 +1,27 @@
+from django import forms
+from django.core.mail.message import EmailMessage
+from .models import Produto
+
+class ContatoForm(forms.Form):
+    nome = forms.CharField(label='Nome', max_length=100)
+    email = forms.EmailField(label='E-mail', max_length=100)
+    assunto = forms.CharField(label='Assunto', max_length=120)
+    mensagem = forms.CharField(label='Mensagem', widget=forms.Textarea())
+
+    def send_mail(self):
+        nome = self.cleaned_data['nome']
+        email = self.cleaned_data['email']
+        assunto = self.cleaned_data['assunto']
+        mensagem = self.cleaned_data['mensagem']
+        conteudo = f'Nome:{nome}\nEmail:{email}\nAssunto:{assunto}\nMensagem:{mensagem}'
+        mail =EmailMessage(subject='Email enviado pelo sistema',
+                           body=conteudo,
+                           from_email='contao@seudominio.com.br',
+                           to=['contao@seudominio.com.br',],
+                           headers={'reply-to':email})
+        mail.send()
+
+class ProdutoModelForm(forms.ModelForm):
+    class Meta:
+        model = Produto
+        fields = ['nome', 'preco', 'estoque', 'imagem']
