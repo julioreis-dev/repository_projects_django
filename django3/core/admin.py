0b8920f1f6951aecb972259c290025e9cafd1c75
Julio dos Reis Firmino <67196397+julioreis-dev@users.noreply.github.com>
2021-04-20 22:16:37 -0300

    Add files via upload

---
+++ django3/core/admin.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+from .models import Cargo, Servico, Funcionario, Features
+
+@admin.register(Cargo)
+class CargoAdmin(admin.ModelAdmin):
+    list_display = ('cargo', 'ativo', 'modificado')
+
+@admin.register(Servico)
+class ServicoAdmin(admin.ModelAdmin):
+    list_display = ('servico', 'icone', 'ativo', 'modificado')
+
+@admin.register(Funcionario)
+class FuncionarioAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'cargo', 'ativo', 'modificado')
+
+@admin.register(Features)
+class FeaturesAdmin(admin.ModelAdmin):
+    list_display = ('nome_feature', 'ativo', 'modificado')
